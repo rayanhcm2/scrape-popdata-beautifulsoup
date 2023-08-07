587f7f25d75a1c5f66300d69b0ac16ed461986f0
rayanhcm2 <rayanehm34@gmail.com>
2023-08-07 22:13:01 +0200

    I added comments and wrote the README file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# scrape-popdata-pythonbeautifulsoup
+# World Population Data Scraper
+Ce script Python permet d'extraire les données de population par pays à partir du site web Worldometer (https://www.worldometers.info/world-population/population-by-country/) et de les enregistrer dans un fichier CSV et un fichier Excel. Il utilise les bibliothèques requests, BeautifulSoup et pandas.
+
+## Prérequis
+Python 3.x (disponible sur https://www.python.org/)
+Les bibliothèques Python : requests, BeautifulSoup, pandas, openpyxl (installez-les via pip si ce n'est pas déjà fait)
+## Comment exécuter le script
+ - Clonez ce dépôt GitHub ou téléchargez le fichier main.py.
+
+ - Ouvrez un terminal/commande et accédez au répertoire où se trouve le fichier main.py.
+
+ - Exécutez le script en utilisant la commande suivante : python main.py
+Le script extraira les données de population à partir du site Worldometer, les enregistrera dans les fichiers data.csv (format CSV) et data.xlsx (format Excel) dans le même répertoire.
+
+## Note :
+Assurez-vous que vous avez une connexion Internet active au moment de l'exécution du script, car il récupère les données en ligne.
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import openpyxl
+# URL de la page web à scraper
 url = "https://www.worldometers.info/world-population/population-by-country/"
+
+# Envoi d'une requête GET à l'URL pour obtenir la page HTML
 response = requests.get(url=url)
-soup = BeautifulSoup(response.content,'html.parser')
-rows = soup.find('table',{'id' :'example2'}).tbody.find_all('tr')
-list_country=[]
+
+# Initialisation de l'objet BeautifulSoup pour analyser le contenu HTML
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Recherche de la table contenant les données de population
+table = soup.find('table', {'id': 'example2'})
+
+# Extraction des lignes de la table (les pays et leurs données)
+rows = table.tbody.find_all('tr')
+
+# Liste pour stocker les données des pays
+list_country = []
+
+# Parcours des lignes pour extraire les données
 for row in rows:
-    dic= {}
+    dic = {}
     dic['Country (or dependency)'] = row.find_all('td')[1].text
     dic['Population'] = row.find_all('td')[2].text
     dic['Yearly change'] = row.find_all('td')[3].text
@@ -17,9 +32,15 @@ for row in rows:
     dic['Migrants  net'] = row.find_all('td')[7].text
     dic['Fert. Rate'] = row.find_all('td')[8].text
     dic['Med. Age'] = row.find_all('td')[9].text
-    dic['Med. Age'] = row.find_all('td')[10].text
-    dic['Urban.  Pop %'] = row.find_all('td')[11].text
+    dic['Urban.  Pop %'] = row.find_all('td')[10].text
     dic['World  Share'] = row.find_all('td')[11].text
     list_country.append(dic)
+
+# Création d'un DataFrame pandas à partir des données extraites
 df = pd.DataFrame(list_country)
-df.to_csv('data.csv',index=False)
+
+# Enregistrement du DataFrame au format CSV
+df.to_csv('data.csv', index=False)  # Enregistrement au format CSV
+
+# Enregistrement du DataFrame au format Excel
+df.to_excel('data.xlsx', index=False)  # Enregistrement au format Excel
